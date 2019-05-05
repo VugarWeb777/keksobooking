df48173921facf6e2777e0ce78e41884eb485b3d
VugarWeb777 <vugarguliev333@gmail.com>
2019-05-05 21:07:15 +0300

    module 7 task 3 - price and type filtering

--- js/filter.js
+++ js/filter.js
@@ -9,30 +9,82 @@
 
     var offers = [];
 
+    var priceVal;
+    var filtersDefaults = {
+        'housing-type': 'any',
+        'housing-price': 'any',
+        'housing-rooms': 'any',
+        'housing-guests': 'any',
+        'filter-wifi': false,
+        'filter-dishwasher': false,
+        'filter-parking': false,
+        'filter-washer': false,
+        'filter-elevator': false,
+        'filter-conditioner': false
+    };
+
     window.GetOffers = function (data) {
         offers = data;
         window.AppendOffers(data);
     };
 
-    var FilterType = function(){
-        window.AppendOffers(offers.slice().filter(function (it) {
-            return it.offer.type === "house";
+
+    var FilterType = function (evt) {
+        priceVal = true;
+
+        window.filteredType = offers.slice().filter(function (it) {
+            switch (evt.target.value) {
+                case it.offer.type :
+                    return it.offer.type;
+                case filtersDefaults["housing-type"] :
+                    return it.offer.type;
+            }
         }).map(function (value) {
-            console.log(value);
             return value;
-        }))
+        });
+        window.AppendOffers(filteredType);
+        return filteredType;
     };
 
-    var FilterPrice = function(){
-        window.AppendOffers(offers.slice().filter(function (it) {
-            return it.offer.price >= 50000;
-        }).map(function (value) {
-            console.log(value);
-            return value;
-        }))
+    var FilterPrice = function (evt) {
+        var middlePrice = 10000;
+        var maxPrice = 50000;
+        priceVal = false;
+
+        if (priceVal === false) {
+            var filterPrice = offers.slice().filter(function (it) {
+                switch (evt.target.value) {
+                    case "middle" :
+                        return it.offer.price >= middlePrice && it.offer.price <= maxPrice;
+                    case "low" :
+                        return it.offer.price <= middlePrice;
+                    case "high" :
+                        return it.offer.price >= maxPrice;
+                }
+            }).map(function (value) {
+                return value;
+            });
+            window.AppendOffers(filterPrice);
+        }
+
+        if (priceVal === true) {
+            var filteredPriceWithType = window.filteredType.slice().filter(function (it) {
+                switch (evt.target.value) {
+                    case "middle" :
+                        return it.offer.price >= middlePrice && it.offer.price <= maxPrice;
+                    case "low" :
+                        return it.offer.price <= middlePrice;
+                    case "high" :
+                        return it.offer.price >= maxPrice;
+                }
+            }).map(function (value) {
+                return value;
+            });
+            window.AppendOffers(filteredPriceWithType);
+        }
     };
 
-    housingType.addEventListener("change",FilterType);
-    housingPrice.addEventListener("change",FilterPrice);
 
+    housingType.addEventListener("change", FilterType);
+    housingPrice.addEventListener("change", FilterPrice);
 })();
